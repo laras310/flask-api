3aa26f8b012a007c4b84d3d163ed7049799645c4
Laras310 <nurullarasati310@gmail.com>
2022-05-29 09:30:27 +0700

    .

---
+++ server/app.py
@@ -0,0 +1,13 @@
+from flask import Flask,jsonify,redirect
+import json
+
+app = Flask(__name__)
+
+@app.route('/products_list')
+def products_list():
+    f = open ('products.json', "r")
+    products = json.loads(f.read())
+    return jsonify(products)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=19002, debug=True)
